193ffd3547776a8f92f45a53b28ecaf200318198
truong-tiffany <ttruong@cmgfi.com>
2026-01-15 15:17:12 -0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -10,4 +11,5 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  };
 });
